f62ed4c9cf3c7f4dac569fcb47d9feda78aac512
Shane Smith <shane.smith.baz@gmail.com>
2011-11-19 13:43:45 -0500

    Twilio menu test

--- application/classes/controller/twilio.php
+++ application/classes/controller/twilio.php
@@ -7,6 +7,8 @@ class Controller_Twilio extends Controller {
 	 */
 	protected $twiml;
 
+	protected $auto_respond = true;
+
 	public function action_index() {
 
 		die("nuthin...");
@@ -15,13 +17,57 @@ class Controller_Twilio extends Controller {
 
 	public function action_callme() {
 
-		Twilio::instance()->call('+16138182762', "http://63.141.238.195/twilio/answered");
+		Twilio::instance()->call('+16138182762', "http://63.141.238.195/twilio/main");
 
 	}
 
-	public function action_answered() {
+	public function action_main() {
 
-		$this->twiml->say("woot");
+		if ($this->request->post()) {
+
+			switch ($this->request->post('Digits')) {
+				case '1':
+					return $this->action_feed();
+				case '9':
+						return $this->action_hell();
+				default:
+					$this->twiml->say("What the hell, that wasn't even a choice, try again numb nuts.");
+			}
+
+
+		} else {
+
+			$this->twiml->say("Welcome to Talking Reports!");
+
+		}
+
+
+
+		$gather = $this->twiml->gather(array('numDigits' => 1));
+
+		$gather->say("Press 1 to listen to a live feed of your sales.");
+
+		$gather->say("Press 9 to go to hell.");
+
+	}
+
+	public function action_feed() {
+
+	}
+
+	public function action_hell() {
+
+		if ($this->request->post()) {
+
+			$this->twiml->say("Go to hell.", array('loop' => $this->request->post('Digits')));
+
+			$this->twiml->redirect("/twilio/main");
+
+		} else {
+
+			$this->twiml->gather(array('numDigits' => 1))->say('How many times would you like to go to hell?');
+
+		}
 
 	}
 
@@ -36,8 +82,8 @@ class Controller_Twilio extends Controller {
 	public function after() {
 
 		if ($this->auto_respond === TRUE) {
-			$this->request->headers['Content-Type'] = File::mime_by_ext('xml');
-			$this->request->response = (string)$this->twiml;
+			$this->response->headers('Content-Type', File::mime_by_ext('xml'));
+			$this->response->body((string)$this->twiml);
 		}
 
 		parent::after();

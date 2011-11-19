db3b776a2b800ea9f9750cbbac7b1ba259c8d204
Luckner <lucknerjb@tekcastdesigns.ca>
2011-11-19 12:17:32 -0500

    Modified template view to show content in content area and created a backup default view

--- application/views/app/index.php
+++ application/views/app/index.php
@@ -1,1 +1,8 @@
-TEST PAGE - message: {{message}} 1
+<strong>Install or Login</strong>
+
+The URL of the shop - or just the .myshopify.com subdomain
+
+<form action="/app/authorize" method="post">
+	<input type="text" id="shop_name" name="shop_name" size="120" maxlength="255" style="width:200px;" />
+	<input type="submit" id="auth_shop" name="auth_shop" value="Install or Login" />
+</form>
---
+++ application/views/default.php
@@ -0,0 +1,137 @@
+    <section id="content">
+        <div class="container_12">
+            <div class="padding">
+                <div class="wrapper margin-bot1">
+                    <article class="grid_12 alpha">
+                        <!-- faded slider begin -->
+                        <div id="faded">
+                              <ul>
+                                <li>
+                                    <div class="faded-box">
+                                        <div class="padding">
+                                            <div class="wrapper indent-bot1">
+                                                <figure class="img-indent3"><img src="images/faded-1.png" alt=""></figure>
+                                                <div class="extra-box">
+                                                    <h2>Services Review</h2>
+                                                    <p class="p2">
+                                                        Dolor sit amet consect etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore magna aliquam erat volutpatt wisi enim adinm veniam quis nostrud exerci
+                                                        tation ullamcorper sucipit etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore.
+                                                    </p>
+                                                     <a class="button-1" href="#">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="faded-box">
+                                        <div class="padding">
+                                            <div class="wrapper indent-bot1">
+                                                <figure class="img-indent3"><img src="images/faded-2.png" alt=""></figure>
+                                                <div class="extra-box">
+                                                    <h2>Services Review</h2>
+                                                    <p class="p2">
+                                                        Dolor sit amet consect etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore magna aliquam erat volutpatt wisi enim adinm veniam quis nostrud exerci
+                                                        tation ullamcorper sucipit etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore.
+                                                    </p>
+                                                     <a class="button-1" href="#">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="faded-box">
+                                        <div class="padding">
+                                            <div class="wrapper indent-bot1">
+                                                <figure class="img-indent3"><img src="images/faded-3.png" alt=""></figure>
+                                                <div class="extra-box">
+                                                    <h2>Services Review</h2>
+                                                    <p class="p2">
+                                                        Dolor sit amet consect etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore magna aliquam erat volutpatt wisi enim adinm veniam quis nostrud exerci
+                                                        tation ullamcorper sucipit etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore.
+                                                    </p>
+                                                    <a class="button-1" href="#">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="faded-box">
+                                        <div class="padding">
+                                            <div class="wrapper indent-bot1">
+                                                <figure class="img-indent3"><img src="images/faded-4.png" alt=""></figure>
+                                                <div class="extra-box">
+                                                    <h2>Services Review</h2>
+                                                    <p class="p2">
+                                                        Dolor sit amet consect etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore magna aliquam erat volutpatt wisi enim adinm veniam quis nostrud exerci
+                                                        tation ullamcorper sucipit etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore.
+                                                    </p>
+                                                    <a class="button-1" href="#">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                              </ul>
+                              <div class="control">
+                                    <a href="#" class="prev">prev</a>
+                                    <a href="#" class="next">next</a>
+                              </div>
+                        </div>
+                        <!-- faded slider end -->
+                    </article>
+                </div>
+                <div class="wrapper">
+                    <article class="grid_12 alpha">
+                        <h3>Our Work is Dedicated to the Prosperity of our Clients!</h3>                       
+                        <div class="wrapper">
+                            <article class="grid_3 alpha">
+                                <figure class="p2"><img src="images/page1-img1.png" alt=""></figure>
+                                <div class="extra-box">
+                                    <h4>Lorem ipsum dolor sit amet consectetuering elit</h4>
+                                    <p class="p2">
+                                    	Lorem ipsum dolor sit amet, consect etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore magna aliquam erat volutpatt wisi enim adinm veniam.
+                                    </p>
+                                     <a class="button-1" href="#">Read More</a>
+                                </div>
+                            </article>
+                            <article class="grid_3">
+                                <figure class="p2"><img src="images/page1-img2.png" alt=""></figure>
+                                <div class="extra-box">
+                                    <h4>Lorem ipsum dolor sit amet consectetuering elit</h4>
+                                    <p class="p2">
+                                    	Lorem ipsum dolor sit amet, consect etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore magna aliquam erat volutpatt wisi enim adinm veniam.
+                                    </p>
+                                    <a class="button-1" href="#">Read More</a>
+                                </div>
+                            </article>
+                            <article class="grid_3">
+                                <figure class="p2"><img src="images/page1-img3.png" alt=""></figure>
+                                <div class="extra-box">
+                                    <h4>Dolor sit amet consecteting elitsed diam nonummy</h4>
+                                    <p class="p2">
+                                    	Lorem ipsum dolor sit amet, consect etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore magna aliquam erat volutpatt wisi enim adinm veniam.
+                                    </p>
+                                    <a class="button-1" href="#">Read More</a>
+                                </div>
+                            </article>
+                            <article class="grid_3 omega">
+                                <figure class="p2"><img src="images/page1-img4.png" alt=""></figure>
+                                <div class="extra-box">
+                                    <h4>Amet consectetueriid diam nonummy nibhincidun</h4>
+                                    <p class="p2">
+                                    	Lorem ipsum dolor sit amet, consect etradipiscing elit sed diam nonummy nieuismod tincidunt ut laoreet dolore magna aliquam erat volutpatt wisi enim adinm veniam.
+                                    </p>
+                                     <a class="button-1" href="#">Read More</a>
+                                </div>
+                            </article>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+

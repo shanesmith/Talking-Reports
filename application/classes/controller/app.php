65ce8f79ff40cf1e3a1a2c5bfa0acacb4aee1373
root <root@lucknerjb-laptop2.(none)>
2011-11-19 11:03:55 -0500

    Added views and controller for /app

---
+++ application/classes/controller/app.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_App extends Controller_Master {
+
+	public $template = 'template/default';
+
+	public function action_index()
+	{
+		$view = View::factory('app/index');
+		$this->template->content = $view->render();
+	}
+
+	public function action_welcome(){
+		$view = new View('app/home');
+		$this->response->body($view->render());
+	}
+
+} // End Welcome

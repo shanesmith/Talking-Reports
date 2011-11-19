04741486527eb2930e92be3f9511ba15af3b7f19
Luckner <lucknerjb@tekcastdesigns.ca>
2011-11-18 21:10:08 -0500

    Added missing /application/cache and /application/logs dirs

---
+++ application/logs/2011/11/18.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-11-18 19:55:39 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+2011-11-18 19:55:39 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
+--
+#0 /home/lucknerjb/Documents/Programming/Shopify/fog.smudel.ca/index.php(109): Kohana_Request->execute()
+#1 {main}

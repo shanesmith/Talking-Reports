faa829622790831bbb83079e48d845b9bc0bb12e
Luckner <lucknerjb@tekcastdesigns.ca>
2011-11-19 12:44:40 -0500

    Added check for authorized store and setup app dashboard

--- application/classes/controller/app.php
+++ application/classes/controller/app.php
@@ -3,6 +3,8 @@
 class Controller_App extends Controller_Master {
 
 	public $template = 'template/default';
+	public $shopify_api_key = '1530c5d5970a56fb6b7c3820df0cdea7';
+	public $shopify_api_secret = 'ac9979b65a71a7654f22e3f85d38fefd';
 
 	public function action_index()
 	{
@@ -13,7 +15,7 @@ class Controller_App extends Controller_Master {
 
 	public function action_welcome(){
 		$view = new View('app/home');
-		$this->template->content = 'test';
+		$this->template->content = 'test1';
 		$this->response->body($view->render());
 	}
 
@@ -24,17 +26,50 @@ class Controller_App extends Controller_Master {
 			$shop_name = $request->post('shop_name');
 
 			if (empty($shop_name)){
-				$this->redirect('app/index');
+				$request->redirect('app/index');
 			}else{
-				//Init Shopify
-				$api = new Session($shop_name, '', $shopify_api_key, $shopify_api_secret);
+				//Shopify install URL
+				$url = shopify_app_install_url($shop_name, $this->shopify_api_key);
+
+				$request->redirect($url);
+				exit();
 			}
 		}else{
 			//GET from Shopify
-			echo 2;
+			//Validate install
+			$request = $this->request;
+
+			//Grab data from GET
+			$shop = $request->query('shop');
+			$token = $request->query('t');
+			$timestamp = $request->query('timestamp');
+			$signature = $request->query('signature');
+
+			$app_installed = shopify_is_app_installed($shop, $token, $timestamp, $signature, $this->shopify_api_secret);
+
+			if (!$app_installed){
+				$content = 'The app is not installed';
+			}else{
+				//Store info in session
+				$session = Session::instance();
+				$session->set('Shopify.shop', $shop);
+				$session->set('Shopify.token', $token);
+				$session->set('Shopify.signature', $signature);
+
+				//Redirect to dashboard
+				$request->redirect('app/dashboard');
+			}
+			die();
 		}
 
 		$view = View::factory('app/authorize');
+		$this->template->content = $view->render();
+	}
+
+	public function action_dashboard(){
+		$session = Session::instance()->as_array();
+		$view = View::factory('app/dashboard');
+		$view->shop_name = $session['Shopify.shop'];
 		$this->template->content = $view->render();
 	}
 
